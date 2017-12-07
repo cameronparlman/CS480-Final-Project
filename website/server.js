799871f90886d6ee8bdd36da66a9b925246a16c8
Joe Kim <kimjoe32@gmail.com>
2017-12-07 17:02:08 -0500

    sorta

--- website/server.js
+++ website/server.js
@@ -13,17 +13,40 @@ app.use(express.static('public'));
 
 function fileFilter (req, file, cb) {
 	if (path.extname(file.originalname).toLowerCase() !== '.avi') {
-		console.log("bad file -- doesn't end with .avi", file.originalname);
+		// console.log("bad file -- doesn't end with .avi", file.originalname);
 		return cb(null, false);
 	}
-	console.log("good file -- ends with .avi");
+	// console.log("good file -- ends with .avi");
 	cb(null, true);
 }
 
-const upload = multer({dest: 'public/uploads/', fileFilter:fileFilter});
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
+  }
+})
+
+const upload = multer({dest: 'public/uploads/', fileFilter:fileFilter, storage:storage});
 
 app.get('/', (req, res) => {
 	res.render('home', { testGreeting: 'Hello world' });
+});
+
+app.get('/finishedVideos', function(req, res) {
+	var fs = require('fs');
+	var files = fs.readdirSync('/finishedVideos');
+	console.log(location.host + files[0]);
+	res.send(location.host + files[0]);
+});
+
+app.post('/upload', upload.single('video-file'), function(req, res, next) {
+	if (!req.file) return next(ERR_NO_FILE);
+	res.redirect('/?error=false&message=' + SUCCESS);
 });
 
 app.post('/upload', upload.single('video-file'), function(req, res, next) {
