17cf878930f13a10b4e210f8347af26923191208
Joe Kim <kimjoe32@gmail.com>
2017-12-09 18:49:16 -0500

    added dynamic video loading

--- website/server.js
+++ website/server.js
@@ -1,15 +1,21 @@
 const SUCCESS = "*File was uploaded";
 const ERR_NO_FILE = "*File was NOT uploaded";
 
+const os = require('os');//os.syscall or something like that. basically exec
 const express = require('express');
 const multer = require('multer');
-var path = require('path')
-
+var path = require('path');
+const fs = require("fs");
 const app = express();
+
+var watcher;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
+
+var fileAvailable = false;
+var returnVideoFilename = "--ERR_NO_VIDEO";
 
 function fileFilter (req, file, cb) {
 	if (path.extname(file.originalname).toLowerCase() !== '.avi') {
@@ -20,14 +26,41 @@ function fileFilter (req, file, cb) {
 	cb(null, true);
 }
 
-const upload = multer({dest: 'uploads/', fileFilter:fileFilter});
+const upload = multer({dest: 'public/uploads/', fileFilter:fileFilter});
 
 app.get('/', (req, res) => {
 	res.render('home', { testGreeting: 'Hello world' });
 });
 
+app.get('/finishedVideos', function(req, res, next) {
+	if (fileAvailable) {
+		console.log("video is available");
+		res.json( {fileAvailable: fileAvailable, 
+							// type: req.file.type, 
+							 src: returnVideoFilename});
+		returnVideoFilename = "--ERR_NO_VIDEO";
+		watcher.close();
+		fileAvailable=false;
+		// console.log("fileAvailable = " + fileAvailable);
+	}
+	else {
+		// console.log("video not available");
+		res.json({fileAvailable: fileAvailable});
+	}
+});
+
 app.post('/upload', upload.single('video-file'), function(req, res, next) {
 	if (!req.file) return next(ERR_NO_FILE);
+
+	watcher = fs.watch("public/finishedVideos", function (event, fileName) {
+		console.log("Event: " + event);
+		if (event === "rename") {
+			console.log("  --file has been added: " + fileName);
+			returnVideoFilename = fileName;
+			fileAvailable=true;
+		}
+	});
+
 	res.redirect('/?error=false&message=' + SUCCESS);
 });
 
